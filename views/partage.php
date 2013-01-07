b1f01f843ed6c190dd2a72d95d9997a4b7bf99a2
Emmanuel Pelletier <pelletier.emmanuel@gmail.com>
2013-01-07 15:32:40 +0100

    modif sur le texte de partage qui inclut le titre de l'image si y'a

--- views/partage.php
+++ views/partage.php
@@ -5,20 +5,23 @@
 <div class="share">
 	<h1 class="title">Alors dis-le !</h1>
 	<?php 
-		$sharingSentence = "Sur ".HOST.",";
-		$coolSharingSentences = array(
-			"j'ai arrêté de déprimer",
-			"j'ai remis un peu de joie dans ma vie",
-			"ma vie a changé",
-			"ma vie est devenue plus belle",
-			"joie et bonheur refont partie de mon vocabulaire",
-			"j'ai kiffé ma race",
-		);
-		$sharingSentence .= " ".$coolSharingSentences[mt_rand(0, count($coolSharingSentences)-1)];
-		if (!empty($item)) {
-			$sharingSentence .= " grâce à ".HOST.'/'.$item['slug'];	
+		if (!empty($item['title']) && strlen($item['title']) < 140-strlen('"" via ')-20) {
+			$sharingSentence = '"'.$item['title'].'" via '.HOST.'/'.$item['slug'];
+		} else {
+			$coolSharingSentences = array(
+				"J'ai arrêté de déprimer",
+				"J'ai remis un peu de joie dans ma vie",
+				"Ma vie a changé",
+				"Ma vie est devenue plus belle",
+				"Joie et bonheur refont partie de mon vocabulaire",
+				"J'ai kiffé ma race",
+			);
+			$sharingSentence = $coolSharingSentences[mt_rand(0, count($coolSharingSentences)-1)];
+			if (!empty($item)) {
+				$sharingSentence .= " grâce à ".HOST.'/'.$item['slug'];	
+			}
+			$sharingSentence .= " !";
 		}
-		$sharingSentence .= " !";
 	?>
 	<p>Partage ta joie sur <a class="share-link twitter" href="https://twitter.com/intent/tweet?text=<?php echo urlencode($sharingSentence) ?>" target="_blank">Twitter</a>, <a class="share-link facebook" href="http://www.facebook.com/sharer/sharer.php?u=<?php echo urlencode(HOST.'/'.$item['slug']) ?>&amp;t=jedepri.me" target="_blank">Facebook</a> ou autre part en copiant le texte ci-dessous :</p>
 	<form action="#">
